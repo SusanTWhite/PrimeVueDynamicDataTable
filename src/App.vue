47bf70c8143f5ae95cb5a6d0ecfdb1f317d3cf8a
SusanTWhite <st.white@yahoo.com>
2024-02-07 14:23:46 -0500

    addNew button and dialog

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div><button @click="parentDirect">Click to have Parent call different utility function</button></div>
-    <Button label="Export CSV" @click="visible = true" />
+    <!-- Button label="Export CSV" @click="visible = true" /> -->
     <Dialog v-model:visible="visible" modal header="Download File">
         <div class="flex align-items-center gap-3 mb-5">
           <a :href="link" severity="secondary" @click="visible = false" target="_blank">{{ fileName }}</a>
@@ -9,7 +9,8 @@
     </Dialog>    
     <div>
         <ChildComponent :triggerParentFunction="triggerParentFunction"
-                        :customFunction="parentFunction" 
+                        :customFunction="parentFunction"
+                        :addNew="addNew" 
                         :anotherFunction="anotherParentFunction"
                         :aFancierFunction="searchFunction"
                         :dataObjectValues="dataObjectValues" 
@@ -19,16 +20,53 @@
                         :utilityFunctionName="utilityFunctionName" 
                         :utilityFunctionParams="utilityFunctionParams" />
     </div>
+    <!---->
+    <Dialog v-model:visible="productDialog" :style="{width: '450px'}" header="Product Details" :modal="true" class="p-fluid">
+      <div class="field" v-if="product">
+          <label for="name">Name</label>
+          <InputText id="name" v-model.trim="product.name" required="true" autofocus :class="{'p-invalid': submitted && !product.name}" />
+          <small class="p-error" v-if="submitted && !product.name">Name is required.</small>
+      </div>
+      <div class="field"  v-if="product">
+        <label for="inventoryStatus" class="mb-3">Inventory Status</label>
+        <Dropdown id="inventoryStatus" v-model="product.inventoryStatus" :options="statuses" optionLabel="label" placeholder="Select a Status">
+          <template #value="slotProps">
+            <div v-if="slotProps.value && slotProps.value.value">
+              <Tag :value="slotProps.value.value"  />
+            </div>
+            <div v-else-if="slotProps.value && !slotProps.value.value">
+                <Tag :value="slotProps.value" />
+            </div>
+            <span v-else>
+              {{slotProps.placeholder}}
+            </span>
+          </template>
+        </Dropdown>
+      </div>
+      <div class="formgrid grid">
+        <div class="field col"  v-if="product">
+            <label for="quantity">Quantity</label>
+            <InputNumber id="quantity" v-model="product.quantity" integeronly />
+        </div>
+      </div>
+      <template #footer>
+          <Button label="Cancel" icon="pi pi-times" text @click="hideDialog" />
+          <Button label="Save" icon="pi pi-check" text @click="saveProduct" />
+      </template>
+    </Dialog>
+  <!---->    
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
+import { useToast } from 'primevue/usetoast';
 import dayjs from 'dayjs'
 import { ProductService } from '@services/ProductService.ts';
 import constants from '@helpers/constants.ts'
 import ChildComponent from './ChildComponent.vue';
 import dateHelper from '@helpers/date-helpers';
 
+const toast = useToast();
 const visible = ref(false);
 const result = ref<number | null>(null);
 const dataObjectValues = ref({ property1: 13, property2: 114 });
@@ -36,6 +74,33 @@ const param1 = ref<number>(0);
 const param2 = ref<number>(0);
 const link = ref('./src/hello.html')
 const fileName = ref('Hello.html')
+const product = ref<ProductType>();
+const productDialog = ref(false);
+const submitted = ref(false);
+const statuses = ref([
+  {label: 'success', value: 'success'},
+  {label: 'info', value: 'info'},  
+  {label: 'warning', value: 'warning'},
+  {label: 'danger', value: 'danger'},
+  {label: 'NEW', value: 'info'}
+]);
+
+const getStatusLabel = (status: string) => {
+    switch (status) {
+        case 'success':
+            return 'success';
+        case 'warning':
+            return 'warning';
+        case 'danger':
+            return 'danger';
+        case 'info':
+            return 'info';
+        case 'new':
+            return 'NEW';
+        default:
+            return null;
+    }
+};
 
 const utilityFunctionName = ref('getDateStringFromTimestamp');
 const utilityFunctionParams = ref<any>(null);
@@ -77,6 +142,25 @@ interface ProductType {
     date: string,
     displayDate: string
 }
+
+const initializeProduct = (product?: ProductType): ProductType => {
+	if (!product) {
+		product = {} as ProductType;
+	}
+	product.id = '';
+	product.code = '';
+	product.name = '';
+  product.description = '';
+  product.image = '';
+	product.price = 0;
+	product.category = '';
+	product.quantity = 0;
+	product.inventoryStatus = '';  
+  product.rating = 0;
+	product.date = '';  
+	product.displayDate = '';      
+	return product;
+};
 
 onMounted(() => {
     ProductService.getProductsMini().then((data) => {
@@ -125,4 +209,66 @@ const searchFunction = async (searchValue: string) => {
     //const derivedData = products.value.filter((row) => row.name.includes(searchValue));    
     //return derivedData;
 };
+
+const addNew = () => {
+  product.value = initializeProduct();
+  submitted.value = false;
+  productDialog.value = true;
+};
+
+const hideDialog = () => {
+  productDialog.value = false;
+  submitted.value = false;
+};
+
+const editProduct = (prod: ProductType) => {
+  product.value = {...prod};
+  productDialog.value = true;
+};
+
+const saveProduct = () => {
+  submitted.value = true;
+  if (!product || !product.value) return;
+
+  if (product.value.name.trim()) {
+    if (product.value.id) {
+        //product.value.inventoryStatus = product.value.inventoryStatus.value ? product.value.inventoryStatus.value : product.value.inventoryStatus;
+        products.value[findIndexById(product.value.id)] = product.value;
+        toast.add({severity:'success', summary: 'Successful', detail: 'Product Updated', life: 3000});
+    }
+    else {
+        product.value.id = createId();
+        product.value.code = createId();
+        //product.value.inventoryStatus = 'new';
+        product.value.quantity = product.value.quantity ?? 0;
+        product.value.date = dayjs().format('YYYY-MM-DD');         
+        product.value.displayDate = dayjs().format('MM/DD/YY');      
+        products.value.push(product.value);
+        toast.add({severity:'success', summary: 'Successful', detail: 'Product Created', life: 3000});
+    }
+    productDialog.value = false;
+    //product.value = {}
+    product.value = initializeProduct(); //TODO: Not sure if this is right, added for compilation
+  }
+};
+
+const findIndexById = (id: string) => {
+  let index = -1;
+  for (let i = 0; i < products.value.length; i++) {
+      if (products.value[i].id === id) {
+          index = i;
+          break;
+      }
+  }
+  return index;
+};
+
+const createId = () => {
+    let id = '';
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for ( var i = 0; i < 9; i++ ) {
+        id += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return id;
+}
 </script>
